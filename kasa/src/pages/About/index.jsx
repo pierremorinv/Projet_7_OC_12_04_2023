f6a84d859a4c029773159e00b6301146c8d8304a
Pierre Morin <morinpierrevo@gmail.com>
2023-04-24 12:37:07 +0200

    fix responsive on NavBar component

--- kasa/src/pages/About/index.jsx
+++ kasa/src/pages/About/index.jsx
@@ -3,11 +3,12 @@ import "./style.scss";
 import Collapse from "../../components/Collapse";
 import Banner from "../../components/Banner";
 import useFetch from "../../service/useFetch";
+import Loader from "../../components/Loader";
 
 const About = () => {
   const { data, isLoading, error } = useFetch(`/assets/about.json`);
   return isLoading && !error ? (
-    <div>chargement</div>
+    <Loader />
   ) : (
     <div>
       <Banner className="banner-about" />
